b538dca3f0fc02eb0c815ba8a7cfc826bcaac2d0
Mitani Misumi <mmisumi@f1r5s18.codam.nl>
2025-10-13 18:27:43 +0200

    yuh

--- rank4/vbc.c
+++ rank4/vbc.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
 
 #define NUM -1
 #define SIGN -2
@@ -34,57 +35,90 @@ int parenthesis(char *argv)
 			a--;
 		}
 		if (a < 0){
-			printf("Unexpected end of input\n");
+			printf("Unexpected token '%c'\n", argv[i]);
 			return 1;
 		}
+		i++;
 	}
 	if (a != 0){
+		printf("Unexpected end of input\n");
 		return 1;
 	}
 	return 0;
 }
 
-int order(char *argv)
+int expecting_num(char c)
 {
-	int expecting;
-	int i = 1;
-
-	if (!isdigit(argv[0]) || argv[i] != '('){
+	if (c == '\0'){
+		return 0;
+	}
+	if (isdigit(c) || c == '('){
 		return 1;
 	}
-	while(argv[i])
-	{
-		if (argv[i] == '(' || argv[i] == ')'){
-			expecting = NUM;
-		}
-		if (isdigit(argv[i])){
-			if (expecting != NUM){
-				return 1;
-			}
-			expecting = SIGN;
-		}
-		if (argv[i] == '+' || argv[i] == '*'){
-			if (expecting != SIGN){
-				return 1;
-			}
+	return 0;
+}
 
-		}
-
+int expecting_sign(char c)
+{
+	if (c == '\0'){
+		return 0;
 	}
+	if (c == '+' || c == '*' || c == ')'){
+		return 1;
+	}
+	return 0;
+}
 
+
+int order(char *argv)
+{
+	int expecting = NUM;
+	int i = 0;
+	
+	if (!argv[1] && !isdigit(argv[0])){
+		return 1;
+	}
+	while (argv[i])
+	{
+		if (expecting == NUM){
+			if (!expecting_num(argv[i])){
+				printf("Unexpected end of input\n");
+				return 1;
+			}
+			if (argv[i] != '('){
+				expecting = SIGN;
+			}
+		}
+		else if (expecting == SIGN){
+			if (!expecting_sign(argv[i])){
+				printf("Unexpected end of input\n");
+				return 1;
+			}
+			if (argv[i] != ')'){
+				expecting = NUM;
+			}
+		}
+		if (!argv[i + 1] && (argv[i]) == '+' || argv[i] == '*'){
+			printf("Unexpected end of input\n");
+			return 1;
+		}
+		i++;
+	}
+	return 0;
 }
 
 int parse(char *argv)
 {
-	if (unexpected_tokens(argv) == 1){
+	if (unexpected_token(argv) == 1){
 		return 1;
 	}
 	if	(parenthesis(argv) == 1){
 		return 1;
 	}
-	if (operation(argv) == 1){
+	if (order(argv) == 1){
 		return 1;
 	}
+	printf("zero errors\n");
 	return 0;
 }
 
